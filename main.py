73630334ee2e2f331f4a4e5b98ec9255957000e9
AlexBehrens2023 <816119@mystma.org>
2021-09-21 15:09:05 -0500

    2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #WRITE YOUR CODE IN THIS FILE
 def sumDiff(x, y):
     #Math
-    return (x + y - x - y)
+    return ((x + y) - (x - y))
 
-sumDiff(10, 5)
-sumDiff(17, 2)
-sumDiff(123, 99)
+print(sumDiff(10, 5))
+print(sumDiff(17, 2))
+print(sumDiff(123, 99))
